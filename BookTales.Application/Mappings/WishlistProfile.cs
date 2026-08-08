ca5ca8ea90e7bff0ec26c2a547ae190a8f17bccb
thasmeer <thasmeer40@gmail.com>
2026-08-08 23:10:11 +0530

    feat: implement wishlist management APIs

---
+++ BookTales.Application/Mappings/WishlistProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BookTales.Application.DTOs.Wishlist;
+using BookTales.Domain.Entities;
+
+namespace BookTales.Application.Mappings;
+
+public class WishlistProfile : Profile
+{
+    public WishlistProfile()
+    {
+        CreateMap<Wishlist, WishlistResponseDto>()
+            .ForMember(
+                dest => dest.Items,
+                opt => opt.MapFrom(src => src.WishlistItems));
+
+        CreateMap<WishlistItem, WishlistItemResponseDto>()
+            .ForMember(
+                dest => dest.BookTitle,
+                opt => opt.MapFrom(src =>
+                    src.Book != null ? src.Book.Title : string.Empty))
+            .ForMember(
+                dest => dest.Price,
+                opt => opt.MapFrom(src =>
+                    src.Book != null ? src.Book.Price : 0))
+            .ForMember(
+                dest => dest.ImageUrl,
+                opt => opt.MapFrom(src =>
+                    src.Book != null ? src.Book.ImageUrl : string.Empty));
+    }
+}
